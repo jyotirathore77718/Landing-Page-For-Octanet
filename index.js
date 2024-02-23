320ee728a4bc1ce4c5809c5a1a328fe4920c52ff
jyotirathore77718 <jyotirathore77718@gmail.com>
2024-02-22 22:02:38 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function openNavbar() {
+	document.getElementById("sideNavigationBar")
+		.style.width = "50%";
+}
+function closeNavbar() {
+	document.getElementById("sideNavigationBar")
+		.style.width = "0%";
+}
+ 374 changes: 374 additions & 0 deletions374  
+Landing page/style.css
